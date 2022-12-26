cf7e4e380b4d59ae0f5dc3cbf67b45497f024117
berkterek <42406263+berkterek@users.noreply.github.com>
2022-12-26 17:10:11 +0300

    feat: scene load dal created and game loop done

---
+++ MageStudy/Assets/_GameFolders/Scripts/InGameScripts/Concretes/DataEntities/UnitySceneLoaderDal.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+namespace MageStudy.DataEntities
+{
+    public class UnitySceneLoaderDal : ISceneLoaderDal
+    {
+        public void LoadSceneByName(string sceneName)
+        {
+            var operation = SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+
+    public interface ISceneLoaderDal
+    {
+        void LoadSceneByName(string sceneName);
+    }
+}
--- MageStudy/Assets/_GameFolders/Scripts/InGameScripts/Concretes/Managers/GameManager.cs
+++ MageStudy/Assets/_GameFolders/Scripts/InGameScripts/Concretes/Managers/GameManager.cs
@@ -1,31 +1,36 @@
 using MageStudy.Abstracts.Helpers;
+using MageStudy.DataEntities;
 using UnityEngine;
 
 namespace MageStudy.Managers
 {
     public class GameManager : MonoSingletonDontDestroy<GameManager>
     {
+        ISceneLoaderDal _sceneLoaderDal;
+        
         public event System.Action<int> OnGameEnded;
         
         void Awake()
         {
             SetSingleton(this);
             Application.targetFrameRate = 60;
+
+            _sceneLoaderDal = new UnitySceneLoaderDal();
         }
 
         public void LoadGameScene()
         {
-            
+            _sceneLoaderDal.LoadSceneByName("Game");
         }
 
         public void LoadMenuScene()
         {
-            
+            _sceneLoaderDal.LoadSceneByName("Menu");
         }
 
         public void GameEnded(int score)
         {
             OnGameEnded?.Invoke(score);
         }
-    }    
+    }
 }
